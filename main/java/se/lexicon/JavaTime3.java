2b6e74f0b467ccd27d32d99eaeb91f0239f4b8b6
Daniel Carlsson <unix555@gmail.com>
2023-11-23 19:53:24 +0100

    Uppdate, working

--- main/java/se/lexicon/JavaTime3.java
+++ main/java/se/lexicon/JavaTime3.java
@@ -7,17 +7,18 @@ public class JavaTime3 {
 
 
 
-    public class DateExample {
+
         public static void main(String[] args) {
-            // Get the current date
+           
             LocalDate currentDate = LocalDate.now();
 
-            // Add 10 years and subtract 10 months
+
             LocalDate futureDate = currentDate.plusYears(10).minusMonths(10);
 
-            // Extract and print the month
+
             Month month = futureDate.getMonth();
             System.out.println("Month: " + month);
+            System.out.println("This day 10 years from now - 10 months");
         }
     }
-}
+
