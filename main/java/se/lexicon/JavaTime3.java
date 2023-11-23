054900ea60deb26362c88b130b49ff3d40b9d1df
Daniel Carlsson <unix555@gmail.com>
2023-11-23 20:46:28 +0100

    exercise 7 added

--- main/java/se/lexicon/JavaTime3.java
+++ main/java/se/lexicon/JavaTime3.java
@@ -3,12 +3,17 @@ package se.lexicon;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.Period;
 public class JavaTime3 {
 
 
 
 
         public static void main(String[] args) {
+
+            System.out.println("Task 6");
+            System.out.println("*********");
+
 
             LocalDate currentDate = LocalDate.now();
 
@@ -19,6 +24,23 @@ public class JavaTime3 {
             Month month = futureDate.getMonth();
             System.out.println("Month: " + month);
             System.out.println("This month 10 years from now - 10 months");
+            System.out.println("");
+            System.out.println("Task 7 ");
+            System.out.println("*************");
+
+
+            LocalDate currentDate2 = LocalDate.of(1974,3, 26);
+           
+            Period period = Period.between (currentDate2, currentDate);
+
+
+            int elapsedYears = period.getYears();
+            int elapsedMonths = period.getMonths();
+            int elapsedDays = period.getDays();
+
+            System.out.println("Elapsed Years: " + elapsedYears);
+            System.out.println("Elapsed Months: " + elapsedMonths);
+            System.out.println("Elapsed Days: " + elapsedDays);
         }
     }
 
