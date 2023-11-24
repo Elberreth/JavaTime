5e00f0b869de4d2fd6f716a6c3603b6ef0b20ca2
Daniel Carlsson <unix555@gmail.com>
2023-11-24 19:59:02 +0100

    added task 9

--- main/java/se/lexicon/JavaTime3.java
+++ main/java/se/lexicon/JavaTime3.java
@@ -2,45 +2,61 @@ package se.lexicon;
 
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.Month;
 import java.time.Period;
 public class JavaTime3 {
+//public class TimeExample {
 
 
+    public static void main(String[] args) {
+
+        currentTime3();
+
+        System.out.println("Task 6");
+        System.out.println("*********");
 
 
-        public static void main(String[] args) {
-
-            System.out.println("Task 6");
-            System.out.println("*********");
+        LocalDate currentDate = LocalDate.now();
 
 
-            LocalDate currentDate = LocalDate.now();
+        LocalDate futureDate = currentDate.plusYears(10).minusMonths(10);
 
 
-            LocalDate futureDate = currentDate.plusYears(10).minusMonths(10);
+        Month month = futureDate.getMonth();
+        System.out.println("Month: " + month);
+        System.out.println("This month 10 years from now - 10 months");
+        System.out.println("");
+        System.out.println("Task 7 ");
+        System.out.println("*************");
 
 
-            Month month = futureDate.getMonth();
-            System.out.println("Month: " + month);
-            System.out.println("This month 10 years from now - 10 months");
-            System.out.println("");
-            System.out.println("Task 7 ");
-            System.out.println("*************");
+        LocalDate currentDate2 = LocalDate.of(1974, 3, 26);
+
+        Period period = Period.between(currentDate2, currentDate);
 
 
-            LocalDate currentDate2 = LocalDate.of(1974,3, 26);
-           
-            Period period = Period.between (currentDate2, currentDate);
+        int elapsedYears = period.getYears();
+        int elapsedMonths = period.getMonths();
+        int elapsedDays = period.getDays();
+
+        System.out.println("Since my birthday:");
+        System.out.println("Elapsed Years: " + elapsedYears);
+        System.out.println("Elapsed Months: " + elapsedMonths);
+        System.out.println("Elapsed Days: " + elapsedDays);
 
 
-            int elapsedYears = period.getYears();
-            int elapsedMonths = period.getMonths();
-            int elapsedDays = period.getDays();
+        //cant understand Ex8 at the moment so ex9
 
-            System.out.println("Elapsed Years: " + elapsedYears);
-            System.out.println("Elapsed Months: " + elapsedMonths);
-            System.out.println("Elapsed Days: " + elapsedDays);
-        }
     }
 
+
+public static void currentTime3()
+        {
+            LocalTime currentTime3 = LocalTime.now();
+            System.out.println("***********");
+            System.out.println("CurrentTime = " + currentTime3);
+            System.out.println("***********");
+
+
+        }}
