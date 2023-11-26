3e4c0e65f138706d64cf03fc2b30d53428f24ef8
Daniel Carlsson <unix555@gmail.com>
2023-11-26 19:26:44 +0100

    added task 12

--- main/java/se/lexicon/JavaTime3.java
+++ main/java/se/lexicon/JavaTime3.java
@@ -16,9 +16,10 @@ public class JavaTime3 {
         currentTime3();
         nanoSeconds();
         timeString();
+        currentTime4();
 
         System.out.println("Task 6");
-        System.out.println("*********");
+        System.out.println("");
 
 
 
@@ -32,12 +33,12 @@ public class JavaTime3 {
         System.out.println("Month: " + month);
         System.out.println("This month 10 years from now - 10 months");
         System.out.println("");
-        
+
 
 
 
         System.out.println("Task 7 ");
-        System.out.println("*************");
+        System.out.println("");
 
 
         LocalDate currentDate2 = LocalDate.of(1974, 3, 26);
@@ -63,38 +64,50 @@ public class JavaTime3 {
 public static void currentTime3() {
     LocalTime currentTime3 = LocalTime.now();
     System.out.println("***********");
-    System.out.println("Task 9 below :)");
-    System.out.println("***********");
+    System.out.println("Task 9  ");
+    System.out.println("");
     System.out.println("Time now");
     System.out.println(currentTime3.format(DateTimeFormatter.ofPattern("H:mm")));
-    System.out.println("***********");
+    System.out.println("");
 }
 
 
 static void nanoSeconds(){
     System.out.println("task 10");
+    System.out.println("");
     System.out.println("NanoSeconds");
         LocalTime nanoSeconds = LocalTime.now();
 
 
     System.out.println( nanoSeconds.format(DateTimeFormatter.ofPattern("n")));
-    System.out.println("***********");
+    System.out.println("");
     }
 
-    static void timeString(){
+    static void timeString() {
         System.out.println("Task 11");
-        System.out.println("********");
+        System.out.println("");
         String timeString = "15:30:45";
 
 
+        // Create a DateTimeFormatter to parse the String
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
 
-    // Create a DateTimeFormatter to parse the String
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        // Parse the String and create a LocalTime object
+        LocalTime parsedTime = LocalTime.parse(timeString, formatter);
 
-    // Parse the String and create a LocalTime object
-    LocalTime parsedTime = LocalTime.parse(timeString, formatter);
-
-    // Print the parsed LocalTime
+        // Print the parsed LocalTime
         System.out.println("Parsed Time: " + parsedTime);
+    }
+        public static void currentTime4(){
+            System.out.println("");
+        System.out.println("Task 12 Current time");
+            System.out.println("");
+            LocalTime currentTime4 = LocalTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+            System.out.println(currentTime4.format(DateTimeFormatter.ofPattern("H:mm")));
+            System.out.println("");
 
-        }}
+    }
+
+
+        }
